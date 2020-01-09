85ad989e0e9c24e4aac6e264007cac47971c5c88
Jonathan <joecampillos@gmail.com>
2020-01-09 20:06:29 +0800

    Final Responsive
    Responsive all sections + optimized resources

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,15 @@ window.addEventListener('click', function (e) {
         }
     }
 });
+
+
+///////////////// MOBILE MENU
+
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+    
+    mainNav.classList.toggle('activemenu');
+});
